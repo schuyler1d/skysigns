63e7e246d5ef41d3d576f3fc0254d0694cffe869
Schuyler Duveen <sky@skyb.us>
2011-08-09 22:56:17 -0400

    initial checkin: shapes/paths loading, shapes printing

---
+++ iswa/sky.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python
+#http://www.signbank.org/signpuddle1.6/glyphogram.php?text=AS14c50S14c58S20600S20600S36d00S36d00M41x34S36d00n21xn14S36d00n21x11S20600n22x23S206000x23S14c5017x1S14c58n42x2&pad=10
+import re,sys,os
+
+#
+x = {'n':-1}
+def inc():
+    x['n'] = x['n']+1
+    return x['n']
+
+def find_paths(dir,allpaths={},shapefile=None):
+    cnt = 0
+    attributes = {}
+    elements = {}
+    g2 = []
+    shapes = {}
+    for fname in sorted(os.listdir(dir)):
+        f = open(os.path.join(dir,fname),'r')
+        fkey = fname.split('.')[0]
+        txt = f.read()
+        paths = re.findall(r'(\<(\w+)([^<]+)\/\>)',txt)
+        cnt += len(paths)
+        shapes[fkey] = {"paths":[]}
+        for p in paths:
+            elements[p[1]] = 1
+            if not p[0] in allpaths:
+                attrs = dict(re.findall(r'(\w+)\=\"([^"]*)\"',p[2]))
+                goodattr=''
+                if p[1]=='rect':
+                    goodattr = ','.join((attrs['x'],attrs['y'],
+                                         attrs['width'],attrs['height'],
+                                        ))
+                elif p[1]=='path':
+                    goodattr = re.sub(r'(.\d{3})\d+',r'\1',
+                                      attrs.get('d','SKY').replace("\n"," "))
+                allpaths[p[0]] = [p[1],p[2],inc(),attrs,goodattr]
+            shapes[fkey]["paths"].append(str(allpaths[p[0]][2]))
+        shapes[fkey]["transforms"] = [
+            re.sub(r'(.\d{3})\d+',r'\1',re.sub(r'(\w)\w+\(',r'\1(',t))
+            for t in re.findall(r'<g[^<]+transform="([^"]+)"',txt)]
+        if shapefile:
+            shapefile.write(
+                '$'.join(
+                    (fkey,
+                    ','.join(shapes[fkey]["paths"]),
+                    ';'.join( shapes[fkey]["transforms"] )
+                    ))+"\n")
+        f.close()
+    return (allpaths,shapes,attributes,elements,cnt,len(allpaths))
+
+#print find_paths(sys.argv[1],shapefile=open('hi.txt','w'))
+    
+def find_all_paths(dir):
+    allpaths={}
+    cnt = 0
+    shapefile=open('shapes.txt','w')
+    for dname in sorted(os.listdir(dir)):
+        path = os.path.join(dir,dname)
+        if os.path.isdir(path):
+            d = find_paths(path,allpaths,shapefile)
+            cnt += d[4]
+    pathfile=open('paths.txt','w')
+    def path_val(a,b):
+        return allpaths[a][2]-allpaths[b][2]
+
+    for p in sorted(allpaths, path_val):
+        path = allpaths[p]
+        pathfile.write(
+            '$'.join((
+                str(path[2]),#number
+                path[0][0],#element first letter
+                ('%sxx' % path[3].get('fill',''))[1], #0 or f for fill
+                path[4] #goodattr
+                ))+"\n")
+    return (cnt,len(allpaths))
+
+print find_all_paths(sys.argv[1])
+
+
+
+def bsw2key(bsw):
+    #http://www.signpuddle.net/mediawiki/index.php/Binary_SignWriting
+    return 'S%s%s%s' % (
+        bsw[0], #base
+        hex(int(bsw[1],16)-922)[2:], #fill
+        hex(int(bsw[2],16)-928)[2:], #rot
+        )
+        
+def csw2ksw(csw):
+    "convert from unicode format in spml file to text -- adapted from csw.php"
+    #AS1c100S1ce00S26600M15x43S1c100n9x15S1ce00n7xn43S26600n15xn10
+    #n='negative'  LMR = left, middle, right
+    #A S1c100 S1ce00 S26600 M15x43 S1c100 n9x15 S1ce00 n7xn43 S26600 n15xn10
+    #'pick' search for M15x43
+    def replace(match):
+        ksw = ''
+        usym = match.group(0)
+        return bsw2key([
+                hex(ord(uchar)-int('1d700',16))[2:] #chop off '0x'
+                for uchar in usym])
+        
+    return re.sub(ur'[\U0001D800-\U0001DA8B][\U0001DA9A-\U0001DA9F][\U0001DAA0-\U0001DAAF]',
+                  replace,
+                  csw, 0, re.UNICODE)
+
+        
+    
+
+
+#maybe total is 7M?
+
+class Phrase: #2.9M
+    ('term') #<term> -- when CDATA it's the english, when text, then encoded
+    ('comment') #<text>
+    ('src') #<src> who provided it
+    ('region') #add this
+    ('usr', #creator
+     'cdt', #create time?
+     'mdt', #modified time?
+     'id'
+     )
+
+class PhraseShapes:
+    ('phraseid')
+    ('shapeid')
+    ('x')
+    ('y')
+
+class Shape:
+    #14c07$0,2,4$transform(1) scale(1,1)$scale(2)
+    ('code','id') #14c07
+    ('transform')
+
+class ShapePaths:
+    ('shapecode')
+    ('pathid')
+
+class Path: #30,000 * 80[average path?] = 2.4M
+    ('type','') #rect,path
+    ('attributes','') #
+    ('?id','') #
+
+#
+#<g transform="scale(0.0662393346391,0.0662393346391)">
+#<g transform="translate(0,363) scale(1.509677,-1.509677)">
+# note: scaling after translation
+    
+# Sign writing cheat sheet:
+#    1. mirror perspective 
+#    2. hand shapes: 
+#         palm, up/forward
+#         fist types: closed,open
+#    3. movement: 
+#         arrows: single, double, crossbars
+#         modifiers: ~, dot, etc
+#    4. face, body, shoulders
+#    5. ?asl alphabet
+
+# Android app
+# reverse search
+#   hand(s)
+#   motion (arrows, douple-tap?)
+#   context (face, arm
---
+++ js/skysign.js
@@ -0,0 +1,222 @@
+/*
+window.ss = {x:0}
+s.loadShapes(function(x,y){++window.ss.x;if (window.ss.x % 1000===0) console.log('hi'+window.ss.x+','+y)})
+s.loadPaths();
+svg = jQuery('#svg').svg().svg('get')
+s.insertShape(svg,jQuery('#svg').svg(),'10000',0,0);
+*/
+
+function SkySigns(){}
+
+SkySigns.prototype = {
+    getShape:function(key) {
+        var rv = {'key':key};
+        var shape = localStorage[key];
+        if (shape) {
+            var sparts = shape.split('$');
+            rv.paths = sparts[0].split(',').map(this.getPath,this);
+            rv.transforms = sparts[1].split(';')
+        }
+        return rv;
+    },
+    getPath:function(i) {
+        return this.paths[i];
+    },
+    transform:function(tr) {
+        return (tr.replace('s(','scale(')
+                .replace('t(','translate(').replace('r(','rotate('));
+    },
+    insertShape:function(svg,parent,key,x,y) {
+        var s = this.getShape(key);
+        console.log(s);
+        parent = svg.other(parent,'g',{
+            transform:'translate('+x+','+y+')'
+        });
+        for (var t=0;t<s.transforms.length;t++) {
+            parent = svg.other(parent,'g',{
+                transform:this.transform(s.transforms[t])
+            });
+        }
+        for (var i=0;i<s.paths.length;i++) {
+            var p = s.paths[i];
+            var color = p[1].replace('f','#ffffff').replace('0','#000000');
+            switch(p[0]) {
+            case 'r':
+                var xywh = p[2].split(',');
+                svg.rect(parent,xywh[0],xywh[1],xywh[2],xywh[3],
+                                {fill:color});
+                break;
+            case 'p':
+                svg.path(parent,p[2],{fill:color});
+                break;
+            }
+        }
+    },
+    openDB:function(){
+        this.db = new SkyDB().open();
+    },
+    loadDB:function(cb){
+        var self = this;
+        //self.loadPaths(cb);
+        self.loadShapes(cb);
+    },
+    loadPaths:function(cb) {
+        var self = this;
+        jQuery.ajax({
+            url:'iswa/paths.txt', dataType:'text',
+            success:function(text){
+                self.paths = [];
+                var text_arr = text.split("\n");
+                for(var i=0,l=text_arr.length;i<l;i++) {
+                    self.paths.push(text_arr[i].split('$').slice(1));
+                }
+            }
+        });
+    },
+    loadShapes:function(cb) {
+        var self = this;
+        jQuery.ajax({
+            url:'iswa/shapes.txt', dataType:'text',
+            success:function(text){
+                //self.addTable('shapes',text,self.db,cb);
+                var text_arr = text.split("\n");
+                var next_ten, i=0, l=text_arr.length;
+                next_ten = function() {
+                    for (var m=Math.min(i+10,l);i<m;i++) {
+                        var line = text_arr[i].match(/^(\w+)\$(.+)$/)
+                        localStorage[line[1]] = line[2];
+                    }
+                    setTimeout(next_ten,2000);
+                }
+                next_ten();
+            }
+        });
+    },
+    addTable:function(name,text,db,cb){
+        //doesn't seem to work :-(
+        //db.addmany(name,text.split('$'),cb);
+        //return;
+        var text_arr = text.split("\n");
+        console.log(text_arr.length);
+        for(var i=0,l=text_arr.length;i<l;i++) {
+            if (i % 1000===0) console.log(i);
+            if (text_arr[i].length) {
+                db.add(name,text_arr[i].split('$'),cb)
+            }
+        }
+    }
+}
+
+function SkyDB(){}
+if (window.openDatabase) {
+    ///paths should be in memory (in array)
+    ///shapes can use localStorage since it just needs .get()
+    ///localStorage can be used for word-shape list and word data
+
+    /*
+    db.transaction(function (tx) {
+       tx.executeSql('CREATE TABLE IF NOT EXISTS foo (id unique, text)');
+       tx.executeSql('INSERT INTO foo (id, text) VALUES (1, "synergies")');
+    });
+    tx.executeSql('INSERT INTO foo (id, text) VALUES (?, ?)', [id, userValue]);
+    tx.executeSql('SELECT * FROM foo', [], function (tx, results) {
+       var len = results.rows.length
+       results.rows.item(i).text
+    })
+    */
+    SkyDB.prototype = {
+        tables:{
+            "paths":{
+                add:function(cols,tx,cb){
+                    var color = {'0':0,'f':1,'x':-1};
+                    tx.executeSql('INSERT INTO paths VALUES (?,?,?,?)',
+                                  [parseInt(cols[0],10),cols[1]==='r',color[cols[2]],cols[3] ],
+                                 cb);
+                },
+                all:function(callback,tx){
+                    tx.executeSql('SELECT * FROM paths',[],callback);
+                },
+                removeall:function(callback,tx){
+                    tx.executeSql('DELETE FROM paths',[],callback);
+                }
+            },
+            "shapes":{
+                add:function(cols,tx,cb){
+                    tx.executeSql('INSERT INTO shapes VALUES (?,?,?)',cols,cb);
+                },
+                removeall:function(callback,tx){
+                    tx.executeSql('DELETE FROM shapes',[],callback);
+                }
+            }
+        },
+        tx:function(table,action,cb){
+            var self = this;
+            this.db.transaction(function(tx) {
+                self.tables[table][action](cb,tx,cb);
+            });
+        },
+        add:function(table,cols,callback) {
+            var self = this;
+            this.db.transaction(function(tx) {
+                self.tables[table].add(cols,tx,callback);
+            });
+        },
+        addmany:function(table,rows,callback) {
+            var self = this;
+            this.db.transaction(function(tx) {
+                for(var i=0,l=rows.length;i<l;i++) {
+                    self.tables[table].add(rows[i].split('$'),tx,callback);
+                }
+            });
+        },
+        count:function(callback) {
+            this.db.readTransaction(function(tx) {
+                tx.executeSql('SELECT COUNT(*) FROM shapes',[],function(tx,r) {
+                    callback('shapes',r.rows.item(0)['COUNT(*)']);
+                });
+                tx.executeSql('SELECT COUNT(*) FROM paths',[],function(tx,r) {
+                    callback('paths',r.rows.item(0)['COUNT(*)']);
+                });
+            });
+        },
+        clearall:function(callback) {
+            var self = this;
+            this.db.transaction(function(tx) {
+                self.tables['shapes'].removeall(callback,tx);
+                self.tables['paths'].removeall(callback,tx);
+            });
+        },
+        open:function() {
+            this.db = openDatabase('skysign6','1.0','signbank',60*1024*1024);
+            if (localStorage['signdb_created']) {
+                this.create();
+            }
+            return this;
+        },
+        create:function() {
+            //dbs on filesystem are at:
+            //~/.config/chromium/Default/databases/
+            this.db.transaction(function (tx) {
+                /*
+                tx.executeSql('CREATE TABLE IF NOT EXISTS paths ( '
+                              +'id INTEGER UNIQUE,'
+                              +'isrect BOOLEAN,'
+                              +'color INTEGER,'
+                              +'path TEXT'
+                              +')'
+                             );
+                */
+                tx.executeSql('CREATE TABLE IF NOT EXISTS shapes ( '
+                              +'key TEXT UNIQUE,'
+                              +'paths TEXT,'
+                              +'transforms TEXT'
+                              +')'
+                             );
+                localStorage['signdb_created'] = 'yes';
+            });
+        }
+    };
+}
+
+window.s = new SkySigns();
+s.openDB();
