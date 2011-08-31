8c45a6561ceec1c52d610e95ecd41f6b6fe4b506
Schuyler Duveen <sky@skyb.us>
2011-08-31 17:27:50 -0400

    giving up on custom/conservative svg2canvas. svg arc path to canvas arc is hard.  using canvg code which "just works"

--- js/svg2canvas.js
+++ js/svg2canvas.js
@@ -39,16 +39,64 @@ SvgNormal.prototype = {
         }
     },
     _transform:function(tr) {
-        return (tr.replace('s(','scale(')
-                .replace('t(','translate(').replace('r(','rotate('));
+        return (tr.replace(/s\(/g,'scale(')
+                .replace(/t\(/g,'translate(').replace(/r\(/g,'rotate('));
     }
 }
 
+CanvgViewer = function(){}
+CanvgViewer.prototype = {
+    init:function() {
+        this.canvas = document.getElementById('canvas');
+
+        var self = this;
+        var y = document.createElement('script');y.src = 'js/canvg.js';
+        document.body.appendChild(y);
+        y.onload = function() {
+            self.ctx=self.canvas.getContext('2d');
+        }
+        return this;
+    },
+    clear:function() {
+        this.canvas.width = this.canvas.width;
+        this.ctx.translate(150,150);
+        this.ctx.save();
+    },
+    _transform:SvgNormal.prototype._transform,
+    insertShape:function(shape,x,y,fgcolor) {
+        var svgstr = '';
+        svgstr += '<g transform="translate('+x+','+y+')" >';
+
+        for (var t=0;t<shape.transforms.length;t++) {
+            svgstr += '<g transform="'+this._transform(shape.transforms[t])+'" >';
+        }
+        for (var i=0;i<shape.paths.length;i++) {
+            var p = shape.paths[i];
+            var color = p[1].replace('f','#ffffff').replace('0',fgcolor||'#000000');
+            switch(p[0]) {
+            case 'r':
+                var xywh = p[2].split(',');
+                svgstr += ('<rect x="'+xywh[0]+'" y="'+xywh[1]
+                           +'" width="'+xywh[2]+'" height="'+xywh[3]
+                           +'" fill="'+color+'"/>');
+                break;
+            case 'p':
+                svgstr += ('<path fill="'+color+'" d="'+p[2]+'" />');
+                break;
+            }
+        }
+        for (var t=0;t<shape.transforms.length;t++) {
+            svgstr += '</g>';
+        }
+        svgstr += '</g>'; //original translated
+        this.ctx.drawSvg(svgstr,150,150,300,300);
+    }
+}
 ViewerBoth = function(){}
 ViewerBoth.prototype = {
     init:function() {
         this.svg = new SvgNormal().init();
-        this.canvas = new Svg2Canvas().init();
+        this.canvas = new CanvgViewer().init();
         return this;
     },
     clear:function() {
@@ -61,115 +109,3 @@ ViewerBoth.prototype = {
     }
 }
 
-Svg2Canvas = function(){}
-Svg2Canvas.prototype = {
-    /* signs still at issue: 150, ?barrier,
-    */
-    init:function() {
-        this.canvas = document.getElementById('canvas');
-        this.ctx = this.canvas.getContext('2d');
-        this.clear();
-        return this;
-    },
-    clear:function() {
-        this.canvas.width = this.canvas.width;
-        this.ctx.translate(150,150);
-        this.ctx.save();
-    },
-    str2nums:function(str) {
-        return str.split(/[\s,]+/).map(function(n){return parseFloat(n);});
-    },
-    path:function(ctx,path,fill) {
-        var self = this;
-        this.current = {x:0,y:0};
-        ctx.fillStyle = fill;
-        ctx.strokeStyle = fill; //todo: FILL first (in another function)
-        ctx.lineWidth = 0;
-        ctx.beginPath();
-        path = path.replace(/-?[\d.]+e-\d+/g,'0');
-        path.replace(/([a-zA-Z])\s*([^a-zA-Z]*)/g,function(match,b,numbers,pos,wholestr) {
-            var num_ary = self.str2nums(numbers.replace(/\s+$/,''));
-            var rel = (b == b.toLowerCase());
-            switch(b.toUpperCase()) {
-            case 'M': 
-                self._series(ctx,'moveTo',num_ary.slice(0,2),2,rel);
-                if (num_ary.length > 2) {
-                    self._series(ctx,'lineTo',num_ary.slice(2),2,rel);
-                }
-                break;
-            case 'L': self._series(ctx,'lineTo',num_ary,2,rel);break;
-            case 'C': self._series(ctx,'bezierCurveTo',num_ary,6,rel);break;
-            case 'Z': ctx.closePath();break;
-            case 'A': //steal from:
-                //http://code.google.com/p/canvg/source/browse/trunk/canvg.js#1308
-                self.arc2canvas(ctx,'arcTo',num_ary,2,rel);break;
-            }
-        });
-        ctx.stroke();
-        if (fill) ctx.fill();
-        
-    },
-    arc2canvas:function(ctx,cmd,ary,arglen,rel) {
-        console.log('ARC NOT IMPLEMENTED YET');
-        //see sign for ?150
-        //all cases are circle-arcs and 0 1 1 or 0 0 1  
-        //no rotation (meaningless)
-    },
-    _series:function(ctx,cmd,ary,arglen,rel) {
-        var l = ary.length/arglen;
-        for (var i=0;i<l;i++) { 
-            var coords = ary.slice(i*arglen,i*arglen+arglen);
-            if (rel) {
-                for (var a=0;a<coords.length;a+=2) {
-                    coords[a] += this.current.x;
-                    coords[a+1] += this.current.y;
-                }
-            }
-            ctx[cmd].apply(ctx,coords);
-            this.current.x = coords[coords.length-2];
-            this.current.y = coords[coords.length-1];
-        }
-    },
-    rect:function(ctx,x,y,w,h,fill) {
-        ctx.fillStyle = fill;
-        ctx.fillRect(x,y,w,h);
-    },
-    transform:function(ctx,str) {
-        var self = this;
-        str.replace(/(\w)\(([^\)]+)\)/g,function(match,tfm,nums) {
-            var num_ary = self.str2nums(nums);
-            switch(tfm) {
-            case 't': ctx.translate.apply(ctx,num_ary); break;
-            case 's': ctx.scale.apply(ctx,num_ary); break;
-            case 'r': ctx.rotate(num_ary[0]* (Math.PI / 180.0)); break;
-            }
-        });
-        
-
-    },
-    insertShape:function(shape,x,y,fgcolor) {
-        return this._insertShape(this.ctx,shape,x,y,fgcolor);
-    },
-    _insertShape:function(ctx,shape,x,y,fgcolor) {
-        ctx.save();
-          ctx.translate(x,y);
-          for (var t=0;t<shape.transforms.length;t++) {
-              this.transform(ctx,shape.transforms[t]);
-          }
-          for (var i=0,l=shape.paths.length;i<l;i++) {
-              var p = shape.paths[i];
-              var color = p[1].replace('f','#ffffff').replace('0',fgcolor||'#000000');
-              switch(p[0]) {
-                case 'r':
-                    var xywh = p[2].split(',');
-                    this.rect(ctx,xywh[0],xywh[1],xywh[2],xywh[3],color);
-                    break;
-                case 'p':
-                  //color = color.replace('1111ff','11ff11');
-                    this.path(ctx,p[2],color);
-                    break;
-                }
-          }
-        ctx.restore();
-    }
-};
