12a2ddb6521ba913ad24990c7298faec2345fadc
Schuyler Duveen <sky@skyb.us>
2011-09-05 19:33:53 -0400

    show load progress is in order

--- js/skysign.js
+++ js/skysign.js
@@ -15,28 +15,16 @@ function SkyInterface(){}
 SkyInterface.prototype = {
     init:function(signs,dict,opts) {
         var self = this;
+	var progress = self.progress.bind(self);
         jQuery('#error').append('<li>pre</li>');
-        this.dict = dict.open(opts);
+        this.dict = dict.open(opts,progress);
         this.signs = signs.open(this.dict.db,opts);
-	this.signs.onpaths = function() {
-	    jQuery('#error').append('<li>paths</li>');
-	    self.signs.insertShape(self.viewer,'10000',0,0);
-	}
-        this.viewer = new opts.viewer().init(function() {
-	    jQuery('#error')
-	    .append('<li>canvas</li>')
-	    .children(':last')
-	    .click(function() {
-	       self.viewer.ctx.drawSvg('<circle r="20" cx="20" cy="20" fill="red" />',0,0,100,100);
-	    });
-	    jQuery('#error')
-	    .append('<li>viewer</li>')
-	    .children(':last')
-	    .click(self.signs.onpaths);
-	});
-        jQuery('#error').append('<li>load paths</li>')
-	.children(':last')
-	.click(function(){self.signs.loadPaths();});
+        if (opts.loadpaths) {
+            this.signs.loadPaths(progress); 
+        }
+	this.loadinterface();
+	var debug = this.debug();
+        this.viewer = new opts.viewer().init(debug.onviewer);
 
         jQuery(document.forms.wordsearch).submit(function(evt) {
             evt.preventDefault();
@@ -47,28 +35,62 @@ SkyInterface.prototype = {
             if (this.value.length > 1)
                 self.search(this.value);
         });
-	jQuery('#error').append('<li>middle</li>');
-        jQuery('#ajaxtest .button').click(this.ajax);
-	try {
-        this.checkloaded();
-        self.notloaded('dict',true);
-        if (document.location.search) {
-            console.log(document.location.search.substr(1));
-            setTimeout(function() {
-                self.search(document.location.search.substr(1),true);
-            },1000)
-        }
+	debug.middle();
+        jQuery('#ajaxtest button').click(this.ajax);
+	try { // this will fail if openDatabase returns NULL
+	    this.checkloaded();
 	} catch(e) {
 	    jQuery('#error').append('<li>e:'+e.message+'</li>');
 	}
-	jQuery('#error').append('<li>end</li>');
+
+	if (document.location.search) {
+	    console.log(document.location.search.substr(1));
+	    setTimeout(function() {
+		    self.search(document.location.search.substr(1),true);
+		},1000)
+	}
+	this.paths = {
+	    load:function(cb) { self.signs.loadPaths(cb); }
+	}
+
+	debug.end();
         return this;
+    },
+    debug:function() {
+	var self = this;
+	return {
+	    'end':function() { jQuery('#error').append('<li>end</li>'); },
+	    'middle':function() { jQuery('#error').append('<li>middle</li>'); },
+	    'onviewer':function() {
+		//debug
+		jQuery('#canvastest div').text('viewer loaded')
+		jQuery('#canvastest button')
+		    .click(function() {
+			    self.viewer.ctx.drawSvg('<circle r="20" cx="20" cy="20" fill="red" />',0,0,100,100);
+			});
+		
+	    }
+	}
+    },
+    loadinterface:function() {
+	var loads = {paths:1,signs:1,dict:1};
+	var self = this;
+	var progress = this.progress.bind(this)
+	var decorate = function(what) {
+	    var display = jQuery('#'+what+'test').get(0);
+	    jQuery('button',display).click(function(evt){
+		    console.log(what);
+		    self[what].load(progress);
+		    jQuery(this).addClass('clicked');
+		});
+	}
+	for (a in loads) decorate(a);
     },
     search:function(q,autoshow) {
         var self = this;
-        this.dict.searchTerms(q,function(results) {
+        this.dict.searchTerms(q,function(x) {
             var dom = $('#results').empty().get(0);
-            var t,r = results.rows;
+            var t,r = x.results.rows;
             for (var i=0,l=r.length;i<l;i++) {
                 t = r.item(i);
                 $(dom).append('<li><a href="#" onclick="si.showTerm('+t.entry+');">'+t.term+'</a></li>');
@@ -82,16 +104,16 @@ SkyInterface.prototype = {
     showTerm:function(entry) {
         var self = this;
         self.viewer.clear();//remove any previous sign
-        this.dict.getEntry(entry,function(what,res) {
-            switch(what) {
+        this.dict.getEntry(entry,function(x) {
+		switch(x.type) {
             case 'entry': 
-                self.signs.showSign(self.viewer,res.shapes);
-                $('#text').html(res.txt);
+                self.signs.showSign(self.viewer,x.item.shapes);
+                $('#text').html(x.item.txt);
                 break;
             case 'terms':
                 var terms = [];
-                for (var i=0;i<res.rows.length;i++) {
-                    terms.push(res.rows.item(i).term);
+                for (var i=0;i<x.results.rows.length;i++) {
+                    terms.push(x.results.rows.item(i).term);
                 }
                 $('#terms').html(terms.join(', '))
                 break;
@@ -101,39 +123,64 @@ SkyInterface.prototype = {
     },
     checkloaded:function() {
         var self = this;
-        this.signs.loaded(function(yes) {
-            if (yes) { self.signs_loaded=yes; }
-            else self.notloaded('signs',yes);
-        });
-        this.dict.loaded(function(yes) {
-            if (yes) { self.dict_loaded=yes; }
-            else self.notloaded('dict',yes);
-        });
+	var callback = self.progress.bind(self);
+        this.signs.loaded(callback);
+        this.dict.loaded(callback);
     },
-    notloaded:function(what,val) {
+    showloaded:function(x) {
+	this[x.type+'_loaded'] = x.loaded;
+	var display = jQuery('#'+x.type+'test').get(0);
+	console.log(x.type);
+	console.log(display);
+	jQuery('.progress',display).show();
+	jQuery('.bar',display).css('width','100%');
+	jQuery('.msg',display).text('loaded:'+(x.count || x.loaded));
+    },
+    notloaded:function(x) {
         var self = this;
-        var error = ($('#error')
-                     .append('<li class="load">Load '+what+'</li>')
-                     .get(0).lastChild);
-        $(error).click(function(evt){
-		self[what].load(function() {self.progress.apply(self,arguments);});
-		jQuery(this).addClass('clicked');
-	    });
+	console.log(x.type);
+	var display = jQuery('#'+x.type+'test').get(0);
+	console.log(display);
+	jQuery('.msg',display).text('not loaded');
+	var what = x.type;
         
     },
-    progress:function(val,results,what) {
-	if (typeof val==='number') {
-	    console.log('max number:'+val);
-	    this._prog_max = val;
-	    jQuery('#bar').css('width',3).empty();
-	    jQuery('#progress').show();
-	} else if (results != 0 && results%1000===0) {
-	    jQuery('#bar').css('width',parseInt(100*results/this._prog_max,10));
-	    console.log('bar:'+results);
-	} else if (results === this._prog_max-2) {
+    _prog_max:{},
+    progress:function(x) {
+	if (x.total) {//max
+	    console.log('max number:'+x.type+':'+x.total);
+	    this._prog_max[x.type] = x.total;
+	    var display = jQuery('#'+x.type+'test').get(0);
+	    jQuery('.bar',display).css('width',3).empty();
+	    jQuery('.progress',display).show();
+	    jQuery('.msg',display).text('request loading ...'+x.total);
+	}
+	var max = this._prog_max[x.type];
+	if (x.index && x.index%500===0) {//count
+	    var display = jQuery('#'+x.type+'test').get(0);
+	    jQuery('.bar',display).css('width',parseInt(100*x.index/max,10));
+	    console.log('bar:'+x.type+':'+x.index);
+	    jQuery('.msg',display).text('request loading: '+x.index+'/'+max);
+	}
+	if (max && x.index==max-1) {
+	    x.loaded = x.index+1;
+	    this.showloaded(x);
+	}
+	if (typeof x.loaded !== 'undefined') {
+	    if (x.loaded) this.showloaded(x);
+	    else this.notloaded(x);
+	}
+	if (x.log) {
+	    jQuery('#error').append('<li>'+x.log+'</li>');
+	}
+	if (x.error) {
+	    jQuery('#error').append('<li>ERROR: '+(x.error.message || x.error)+'</li>');
+	}
+	
+	/* TODO: else if (results === this._prog_max-2) {
 	    //-2: who knows why, but from l=7412, we seem to get to 7410
 	    jQuery('#bar').css('width','100px').append('complete');
-	} 
+	    } */
     },
     ajax:function() {
         jQuery('#ajaxtest').append('<span>start</span>');
@@ -205,12 +252,9 @@ SkySigns.prototype = {
         }
         return rv;
     },
-    open:function(db,opts){ 
+    open:function(db,opts,logback){ 
         this.db = db; 
         this.opts = opts;
-        if (opts.loadpaths) {
-            this.loadPaths(); 
-        }
         return this; 
     },
     load:function(cb){ this.db.create(); this.loadShapes(cb); },
@@ -221,31 +265,30 @@ SkySigns.prototype = {
 	    jQuery('#error').append('<li>loaded.db:'+(typeof this.db.haveShapes)+'</li>');
 	    this.db.haveShapes(cb);
         } else {
-            cb(Boolean(localStorage['10000'] && localStorage['38b07']));
+            cb({'loaded':(Boolean(localStorage['10000'] && localStorage['38b07'])),'type':"signs"});
         }
-    },
-    connectPaths:function() {
-        if (window.SignPaths) this.paths = window.SignPaths;
-	if (this.onpaths) this.onpaths();
     },
     loadPaths:function(cb) {
         //needs to happen every page-load
         var self = this;
+	cb = cb || function(){};
         jQuery.ajax({
             url:this.opts.base_path+'iswa/paths.txt', dataType:'text',
             success:function(text){
                 self.paths = [];
                 var text_arr = text.split("\n");
+                cb({'total':text_arr.length,'type':"paths"});
                 for(var i=0,l=text_arr.length;i<l;i++) {
                     self.paths.push(text_arr[i].split('$').slice(1));
                 }
-                if (typeof cb==='function') cb(text_arr.length);
-		jQuery('#error').append('<li>gotpaths.txt</li>');
+                cb({'loaded':self.paths.length,'type':"paths"});
             },
             error:function(xhr,status,error) {
 		jQuery('#error').append('<li>nopaths:'+error+'</li>');
+		cb({'error':error,'type':"paths"});
             }
         });
+	cb({'event':"request",'type':"paths"});
     },
     loadShapes:function(cb) {
         var self = this;
@@ -254,18 +297,20 @@ SkySigns.prototype = {
             success:function(text){
                 var text_arr = text.split("\n");
                 var next_ten, i=0, l=text_arr.length;
-                cb(l-1, [], 'shapes');
+                cb({'total':l-1, 'type':"signs"});
                 next_ten = function() {
-                    for (var m=Math.min(i+10,l);i<m;i++) {
+		    var lines = [];
+                    for (var m=Math.min(i+100,l);i<m;i++) {
                         var line = text_arr[i].match(/^(\w+)\$(.+)$/)
-                        if (line) {
-                            if (!self.db) {
-                                localStorage[line[1]] = line[2];
-                            } else {
-                                self.db.addShape(line.slice(1),cb,i);
-                            }
-                        }
-                    }
+			if (line) lines.push(line.slice(1))
+		    }
+		    if (!self.db) {
+			while (ln = lines.shift()) {
+			    localStorage[ln[0]] = ln[1];
+			}
+		    } else {
+			self.db.addShapes(lines,cb,i);
+		    }
                     if (i < l) {
                         next_ten();
                     }
@@ -273,7 +318,7 @@ SkySigns.prototype = {
                 next_ten();
             },
             error:function(xhr,status,error) {
-		jQuery('#error').append('<li>noshapesx:'+status+error+'</li>');
+		cb({'log':'noshapex:'+status,'error':error});
             }
         });
     }
@@ -281,8 +326,8 @@ SkySigns.prototype = {
 
 function SkyDictionary(){}
 SkyDictionary.prototype = {
-    open:function(opts){
-        this.db = new SkyDB().open();
+    open:function(opts,logback){
+        this.db = new SkyDB().open(logback);
         this.opts = opts;
         return this;
     },
@@ -292,7 +337,7 @@ SkyDictionary.prototype = {
 
     },
     loaded:function(cb) {
-	jQuery('#error').append('<li>loadedx.db:'+(typeof this.db.count)+'</li>');
+	cb({'log':'loadedx.db:'+(typeof this.db.count)});
         this.db.count(cb);
     },
     load:function(cb) {
@@ -307,14 +352,15 @@ SkyDictionary.prototype = {
 		jQuery('#error').append('<li>noentries:'+error+'</li>');
             }
         });
+	cb({'event':"request",'type':"dict"});
     },
     addTable:function(name,text,db,cb){
         var text_arr = text.split("\n"), 
             l=text_arr.length;
-        cb(l-1,[],'signs');
+        cb({'total':l-1,'type':"dict"});
         for(var i=0;i<l;i++) {
             if (text_arr[i].length) {
-                db.add(name,text_arr[i].split('$'),cb,i)
+                db.addEntries(name,[text_arr[i].split('$')],cb,i)
             }
         }
     }
--- js/skystore.js
+++ js/skystore.js
@@ -16,44 +16,65 @@ if (window.openDatabase) {
        results.rows.item(i).text
     })
     */
+    //addcallback(), 
+    //countcallback(), 
+    //getcallback()
     SkyDB.prototype = {
         searchTerms:function(q,cb) {
             this.db.transaction(function(tx) {
                 tx.executeSql("SELECT * FROM terms WHERE term LIKE '"+q.replace("'","''")+"%' ORDER BY term",[],
-                              function(tx,res) { cb(res); }
+                              function(tx,res) { cb({'results':res,'type':"terms"}); }
                              );
             });
         },
         getEntry:function(entry,cb) {
             this.db.transaction(function(tx) {
                 tx.executeSql("SELECT * FROM entries WHERE entry=?",[entry],
-                              function(tx,res) { cb('entry',res.rows.item(0)); });
+                              function(tx,res) { cb({'type':"entry",'item':res.rows.item(0)}); });
                 tx.executeSql("SELECT * FROM terms WHERE entry=?",[entry],
-                              function(tx,res) { cb('terms',res); });
+                              function(tx,res) { cb({'type':"terms",'results':res}); });
             });            
         },
         _add:function(tx,cols,callback,i) {
             //cols=[id,terms,code,text]
             var terms = cols[1].split('^');
             for (var t=0;t<terms.length;t++) {
-                tx.executeSql('INSERT INTO terms VALUES (?,?)',[cols[0],terms[t]],callback);
+                tx.executeSql('INSERT INTO terms VALUES (?,?)',[cols[0],terms[t]]);
             }
             tx.executeSql('INSERT INTO entries VALUES (?,?,?)',[cols[0],cols[2],cols[3]],
-			  function() { callback(null,i); });
+			  function() { 
+			      callback({'index':i,'type':"dict",'item':cols}); 
+			  },this.errback(callback));
         },
-        add:function(table,cols,callback,i) {
+        addEntries:function(table,rows,callback,i) {
             var self = this;
-            this.db.transaction(function(tx) {
-		self._add(tx,cols,callback,i);
-            });
-        },
-        addShape:function(cols,callback,i) {
-	    var self = this;
 	    this.db.transaction(function(tx) {
-		tx.executeSql('INSERT INTO shapes VALUES (?,?)',cols,
-			      function() { callback(null,i,cols); });
-            });
+		    for (var j=0;j<rows.length;j++) {
+			var cols = rows[j];
+			self._add(tx,cols,callback,i+j);
+		    }
+		});
         },
+	errback:function(cb) {
+	    return function(tx, err) {
+		cb({log:'db error',error:err});
+	    }
+	},
+        addShapes:function(shapes,callback,i) {
+	    var self = this;
+	    var getcb = function(i,j,cols,typ) {
+		return function() { 
+		    callback({'index':i+j,'item':cols,'type':typ}); 
+		}
+	    }
+	    this.db.transaction(function(tx) {
+		for (var j=0;j<shapes.length;j++) {
+		    var cols = shapes[j];
+		    tx.executeSql('INSERT INTO shapes VALUES (?,?)',
+				  cols,getcb(i,j,cols,'signs'),self.errback(callback));
+		}
+            });
+	},
 	getShape:function(key,callback) {
 	    var self = this;
 	    console.log(key);
@@ -70,15 +91,15 @@ if (window.openDatabase) {
 	},
 	haveShapes:function(callback) {
 	    var self = this;
-	    jQuery('#error').append('<li>haveShapes:'+this.db+'</li>');
-	    jQuery('#error').append('<li>haveShapes f:'+(typeof this.haveShapes)+'</li>');
+	    callback({'log':'haveShapes:'+this.db});
+	    callback({'log':'haveShapes f:'+(typeof this.haveShapes)});
 	    this.db.transaction(function(tx) {
 		tx.executeSql('SELECT COUNT(*) FROM shapes WHERE key=? OR key=?',
 			      ['10000','38b07'],function(tx,res) {
 				  if (res.rows.item(0)['COUNT(*)'] == 2) {
-				      callback(2);
-				  } else callback(false);
-			      },function(tx,e){callback(false,e);});
+				      callback({'loaded':2,'type':"signs"});
+				  } else callback({'loaded':false,'type':"signs"});
+			      },function(tx,e){callback({'loaded':false,'error':e});});
 	    });
 	},
 	addmany:function(table,rows,callback,i) {
@@ -96,14 +117,15 @@ if (window.openDatabase) {
                 tx.executeSql('DELETE FROM entries',[],callback);
             });
         },
-        open:function() {
+        open:function(logback) {
             //iOS webview actually enforces the quota: seems to max out at 2.75M
-	    jQuery('#error').append('<li>db orig?:'+window.openDatabase_orig+'</li>');
+	    logback = logback || function(){};
+	    logback({'log':'db orig?:'+window.openDatabase_orig});
             try {
                 this.db = openDatabase('skysign11','1.0','signbank',60*1024*1024);
-                jQuery('#error').append('<li>db opening:'+this.db+'</li>');
+		logback({'log':'db opening:'+this.db});
             } catch(e) {
-                jQuery('#error').append('<li>db:'+e.message+'</li>');
+		logback({'log':'db error:'+e.message,'error':e});
             }
             return this;
         },
@@ -114,19 +136,18 @@ if (window.openDatabase) {
                 tx.executeSql("SELECT tbl_name from sqlite_master WHERE type = 'table' and tbl_name='entries'",[],function(tx,results) {
                     if (results.rows.length) {
                         tx.executeSql("SELECT COUNT(*) FROM entries",[],function(tx,res) {
-                            cb(res.rows.item(0)['COUNT(*)']);
+				var count = res.rows.item(0)['COUNT(*)'];
+				cb({'loaded':count,'total':count,'type':"dict"});
                         });
                     } else {
-                        cb(false);
+                        cb({'loaded':false,'type':"dict"});
                     }
                 });
             })            
         },
         create:function() {
-            //dbs on filesystem are at:
-            //~/.config/chromium/Default/databases/
+            //dbs on filesystem are at: ~/.config/chromium/Default/databases/
             this.db.transaction(function (tx) {
-                //entryid-term entryid-shape+pos
                 tx.executeSql('CREATE TABLE IF NOT EXISTS terms ( '
                               +'entry INTEGER,'
                               +'term TEXT'
@@ -139,8 +160,14 @@ if (window.openDatabase) {
                               //TODO: dictionary id, too?
                               +')'
                              );
+		//these may or may not be used;
                 tx.executeSql('CREATE TABLE IF NOT EXISTS shapes ( '
-                              +'key TEXT,'
+                              +'key TEXT UNIQUE,'
+                              +'data TEXT'
+                              +')'
+                             );
+                tx.executeSql('CREATE TABLE IF NOT EXISTS paths ( '
+                              +'id INTEGER UNIQUE,'
                               +'data TEXT'
                               +')'
                              );
